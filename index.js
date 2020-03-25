56dfd6ab97ed530949d6bf33f75bb4d260532248
Vendy Prum <vendy@thinkful.com>
2020-03-25 14:37:39 -0700

    first commit: some of the drills completed

---
+++ index.js
@@ -0,0 +1,24 @@
+// function sort(list) {
+//   for(let i = 2; i < list.length; i++){
+//     let j = i;
+//     while(j > 0 && list[j - 1] > list[j]){
+//       let temp = list[j];
+//       list[j] = list[j - 1];
+//       list[j - 1] = temp;
+//       j--;
+//     }
+//   }
+//   return list;
+// }
+
+// module.export = sort;
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by 0');
+  }
+
+  return a / b;
+}
+
+module.exports = divide;
---
+++ test/drill1.test.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai');
+const supertest = require('supertest');
+const sort = require('../index');
+
+//first drill
+
+// describe('sort function', ()=>{
+//   it('should sort by ascending order',()=>{
+//     const arr1 = [1,3,4];
+//     const expectedAnswer =[4,3,1];
+//     const actualAnswer = sort(arr1);
+//     expect(actualAnswer).to.deep.equal(expectedAnswer);
+//   });
+// });
+
+//second drill: Assertions
